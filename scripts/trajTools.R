8815ef7db4a7c4acda4686f38320970f50e8f9cf
Tim Vaughan <timv@ughan.xyz>
2022-03-17 18:21:09 +0100

    Added subpop support to trajTools.

--- scripts/trajTools.R
+++ scripts/trajTools.R
@@ -8,13 +8,24 @@ library(tidyr)
 library(ggplot2)
 
 parseTrajectory <- function(trajStr) {
-  strValues <- str_split(str_split(trajStr, ",")[[1]], ":", simplify = TRUE)
+  strValues <- str_split(str_split(trajStr, ";")[[1]], ":", simplify = TRUE)
+  vars <- str_split(strValues[1,], "=", simplify=TRUE)[,1]
+  colnames(strValues) <- vars
 
-  vars <- str_split(strValues[1,], "=", simplify=TRUE)[-1,1]
-  res <- tibble(t=as.numeric(str_split(strValues[,1], "=", simplify=TRUE)[,2]))
 
-  for (idx in 1:length(vars))
-      res[vars[idx]] <- as.numeric(str_split(strValues[,idx+1], "=", simplify=TRUE)[,2])
+  res <- as_tibble(strValues) %>%
+      mutate(across(everything(),
+                    function(x) {str_split(x,"=",simplify=TRUE)[,2]})) #%>%
+
+  for (var in vars[-1]) {
+      n <- length(str_split(res[1,var],",",simplify=TRUE))
+      res <- res %>% separate(var, into=paste0(var,"__",0:(n-1)), sep=",")
+  }
+
+  res <- res %>%
+      pivot_longer(!t) %>%
+      separate(name,into=c("population","index"), sep="__") %>%
+      mutate(across(!population, as.numeric))
 
   return(res)
 }
@@ -23,26 +34,23 @@ loadTrajectories <- function(filenames) {
     df <- NULL
 
     for (filename in filenames) {
-        cat(paste("Loading", filename,"..."))
         df_in <- read_tsv(filename, col_types="ic")
 
         df <- NULL
         for (row in 1:(dim(df_in)[1])) {
             trajStr <- df_in[row,2]
-            trajStates <- parseTrajectory(trajStr) %>% pivot_longer(cols=!t, names_to="variable")
+            trajStates <- parseTrajectory(trajStr)
             trajStates$Sample <- df_in$Sample[row]
             df <- bind_rows(df, trajStates)
         }
     }
-    
-    cat("done.\n")
     
     return(df)
 }
 
 plotTrajectories <- function(filenames) {
     ggplot(loadTrajectories(filenames),
-           aes(x=t, y=value, col=variable,
-               group=interaction(variable,factor(Sample)))) +
-        geom_step()
+           aes(x=t, y=value, col=interaction(population, factor(index)),
+               group=interaction(population,factor(index), factor(Sample)))) +
+        geom_step(alpha=0.2)
 }
